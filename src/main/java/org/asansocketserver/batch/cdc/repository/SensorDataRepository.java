700f6767360759d9c979034977ac6bfa990087e4
jangjh0201 <jangjh0201@gmail.com>
2025-01-24 16:47:56 +0900

    [chore] repository key 타입 변경

--- src/main/java/org/asansocketserver/batch/cdc/repository/SensorDataRepository.java
+++ src/main/java/org/asansocketserver/batch/cdc/repository/SensorDataRepository.java
@@ -1,6 +1,7 @@
 package org.asansocketserver.batch.cdc.repository;
 
 import org.asansocketserver.batch.cdc.entity.SensorData;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -8,14 +9,13 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface SensorDataRepository extends MongoRepository<SensorData, String>, SensorDataRepositoryCustom {
+public interface SensorDataRepository extends MongoRepository<SensorData, ObjectId>, SensorDataRepositoryCustom {
     boolean existsByWatchIdAndDate(Long watchId, LocalDate date);
 
-
     List<SensorData> findAllByWatchIdAndDateBetween(Long patientId, LocalDate localDate, LocalDate localDate1);
+
     @Query(value = "{ 'watch_id': ?0, 'date': ?1 }", fields = "{ 'name': 1, 'watch_id': 1, '_id': 1 }")
     Optional<SensorData> findByWatchIdAndDate(Long watchId, LocalDate date);
-
 
     List<SensorData> findAllByNameAndDate(String name, LocalDate now);
 
--- src/main/java/org/asansocketserver/domain/position/repository/BeaconDataRepository.java
+++ src/main/java/org/asansocketserver/domain/position/repository/BeaconDataRepository.java
@@ -8,16 +8,16 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.HashMap;
 import java.util.List;
 
-public interface BeaconDataRepository extends JpaRepository<BeaconData, Long> {
+public interface BeaconDataRepository extends JpaRepository<BeaconData, Integer> {
     List<BeaconData> findAllByPosition(String positionName);
 
-
-//    @Query("SELECT b.position, c.longitude, c.latitude, COUNT(b) FROM Coordinate c LEFT JOIN BeaconData b ON b.position = c.position GROUP BY b.position ,  c.longitude, c.latitude")
-//    List<Object[]> findAllBeaconCount();
+    // @Query("SELECT b.position, c.longitude, c.latitude, COUNT(b) FROM Coordinate
+    // c LEFT JOIN BeaconData b ON b.position = c.position GROUP BY b.position ,
+    // c.longitude, c.latitude")
+    // List<Object[]> findAllBeaconCount();
 
     @Query("SELECT c.position, COUNT(b) FROM Coordinate c LEFT JOIN BeaconData b ON b.position = c.position GROUP BY c.position")
     List<Object[]> findAllBeaconCount();
-
 
     public void deleteAllByImageId(Long imageId);
 
