4cbb18d7dba0c3167d2314c13b547d9d8d6739c2
jangjh0201 <jangjh0201@gmail.com>
2025-03-28 04:15:43 +0900

    [fix] batch 파일 주석 해제 및 어노테이션 삽입

--- src/main/java/org/assansocketserver/domain/sensor/batch/SensorDataJobLauncher.java
+++ src/main/java/org/assansocketserver/domain/sensor/batch/SensorDataJobLauncher.java
@@ -1,55 +1,55 @@
 package org.assansocketserver.domain.sensor.batch;
-// package org.asansocketserver.domain.sensor.batch;
 
-// import org.springframework.batch.core.Job;
-// import org.springframework.batch.core.JobParameters;
-// import org.springframework.batch.core.JobParametersBuilder;
-// import org.springframework.batch.core.launch.JobLauncher;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Qualifier;
-// import org.springframework.scheduling.annotation.Scheduled;
-// import org.springframework.stereotype.Component;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-// @Component
-// public class SensorDataJobLauncher {
+@Component
+public class SensorDataJobLauncher {
 
-// private final JobLauncher jobLauncher;
-// @Qualifier("sensorDataJob")
-// private final Job sensorDataJob;
-// @Qualifier("deleteExpiredSensorDataJob")
-// private final Job deleteExpiredSensorDataJob;
+    private final JobLauncher jobLauncher;
+    @Qualifier("sensorDataJob")
+    private final Job sensorDataJob;
+    @Qualifier("deleteExpiredSensorDataJob")
+    private final Job deleteExpiredSensorDataJob;
 
-// @Autowired
-// public SensorDataJobLauncher(JobLauncher jobLauncher, Job sensorDataJob, Job
-// deleteExpiredSensorDataJob) {
-// this.jobLauncher = jobLauncher;
-// this.sensorDataJob = sensorDataJob;
-// this.deleteExpiredSensorDataJob = deleteExpiredSensorDataJob;
-// }
+    @Autowired
+    public SensorDataJobLauncher(JobLauncher jobLauncher,
+            @Qualifier("sensorDataJob") Job sensorDataJob,
+            @Qualifier("deleteExpiredSensorDataJob") Job deleteExpiredSensorDataJob) {
+        this.jobLauncher = jobLauncher;
+        this.sensorDataJob = sensorDataJob;
+        this.deleteExpiredSensorDataJob = deleteExpiredSensorDataJob;
+    }
 
-// @Scheduled(cron = "0 47 14 * * *")
-// public void runJob() {
-// try {
-// JobParameters jobParameters = new JobParametersBuilder()
-// .addLong("time", System.currentTimeMillis())
-// .toJobParameters();
+    @Scheduled(cron = "0 */1 * * * *")
+    public void runJob() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
 
-// jobLauncher.run(sensorDataJob, jobParameters);
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-// }
+            jobLauncher.run(sensorDataJob, jobParameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-// @Scheduled(cron = "0 21 7 * * *")
-// public void runDeleteExpiredSensorDataJob() {
-// try {
-// JobParameters jobParameters = new JobParametersBuilder()
-// .addLong("time", System.currentTimeMillis())
-// .toJobParameters();
+    @Scheduled(cron = "0 21 7 * * *")
+    public void runDeleteExpiredSensorDataJob() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
 
-// jobLauncher.run(deleteExpiredSensorDataJob, jobParameters);
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-// }
-// }
+            jobLauncher.run(deleteExpiredSensorDataJob, jobParameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/assansocketserver/global/config/BatchConfig.java
+++ src/main/java/org/assansocketserver/global/config/BatchConfig.java
@@ -1,104 +1,95 @@
 package org.assansocketserver.global.config;
-// package org.asansocketserver.global.config;
 
-// import jakarta.persistence.EntityManagerFactory;
-// import org.asansocketserver.batch.cdc.entity.SensorData;
-// import org.asansocketserver.batch.cdc.repository.SensorDataRepository;
-// import org.asansocketserver.batch.cdc.service.SensorDataService;
-// import org.asansocketserver.domain.watch.entity.Watch;
-// import org.asansocketserver.domain.watch.repository.WatchRepository;
-// import org.springframework.batch.core.Job;
-// import org.springframework.batch.core.Step;
-// import
-// org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
-// import org.springframework.batch.core.job.builder.JobBuilder;
-// import org.springframework.batch.core.repository.JobRepository;
-// import org.springframework.batch.core.step.builder.StepBuilder;
-// import org.springframework.batch.item.ItemProcessor;
-// import org.springframework.batch.item.ItemReader;
-// import org.springframework.batch.item.ItemWriter;
-// import org.springframework.batch.item.support.ListItemReader;
-// import org.springframework.batch.repeat.RepeatStatus;
-// import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
-// import
-// org.springframework.batch.support.transaction.ResourcelessTransactionManager;
-// import org.springframework.beans.factory.annotation.Qualifier;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.orm.jpa.JpaTransactionManager;
-// import org.springframework.transaction.PlatformTransactionManager;
+import jakarta.persistence.EntityManagerFactory;
 
-// import java.util.List;
+import org.assansocketserver.batch.cdc.entity.SensorData;
+import org.assansocketserver.batch.cdc.repository.SensorDataRepository;
+import org.assansocketserver.batch.cdc.service.SensorDataService;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
-// @Configuration
-// public class BatchConfig extends DefaultBatchConfiguration {
+import java.util.List;
 
-// @Bean
-// public Job sensorDataJob(JobRepository jobRepository, Step sensorDataStep) {
-// return new JobBuilder("sensorDataJob", jobRepository)
-// .start(sensorDataStep)
-// .build();
-// }
+@Configuration
+public class BatchConfig extends DefaultBatchConfiguration {
 
-// @Bean
-// public Step sensorDataStep(JobRepository jobRepository,
-// ItemReader<SensorData> sensorDataReader,
-// ItemProcessor<SensorData, SensorData> sensorDataProcessor,
-// ItemWriter<SensorData> sensorDataWriter,
-// PlatformTransactionManager transactionManager) {
+    @Bean
+    public Job sensorDataJob(JobRepository jobRepository, @Qualifier("sensorDataStep") Step sensorDataStep) {
+        return new JobBuilder("sensorDataJob", jobRepository)
+                .start(sensorDataStep)
+                .build();
+    }
 
-// return new StepBuilder("sensorDataStep", jobRepository)
-// .<SensorData, SensorData>chunk(new SimpleCompletionPolicy(10),
-// transactionManager)
-// .reader(sensorDataReader)
-// .processor(sensorDataProcessor)
-// .writer(sensorDataWriter)
-// .build();
-// }
+    @Bean
+    public Step sensorDataStep(JobRepository jobRepository,
+            ItemReader<SensorData> sensorDataReader,
+            ItemProcessor<SensorData, SensorData> sensorDataProcessor,
+            ItemWriter<SensorData> sensorDataWriter,
+            PlatformTransactionManager transactionManager) {
 
-// @Bean
-// public Job deleteExpiredSensorDataJob(JobRepository jobRepository, Step
-// deleteExpiredSensorDataStep) {
-// return new JobBuilder("deleteExpiredSensorDataJob", jobRepository)
-// .start(deleteExpiredSensorDataStep)
-// .build();
-// }
+        return new StepBuilder("sensorDataStep", jobRepository)
+                .<SensorData, SensorData>chunk(new SimpleCompletionPolicy(10),
+                        transactionManager)
+                .reader(sensorDataReader)
+                .processor(sensorDataProcessor)
+                .writer(sensorDataWriter)
+                .build();
+    }
 
-// @Bean
-// public Step deleteExpiredSensorDataStep(JobRepository jobRepository,
-// ItemReader<SensorData> sensorDataReader,
-// PlatformTransactionManager transactionManager,
-// SensorDataService sensorDataService) {
-// return new StepBuilder("deleteExpiredSensorDataStep", jobRepository)
-// .tasklet((contribution, chunkContext) -> {
-// sensorDataService.deleteExpiredSensorData();
-// return RepeatStatus.FINISHED;
-// }, transactionManager)
-// .build();
-// }
+    @Bean
+    public Job deleteExpiredSensorDataJob(JobRepository jobRepository,
+            @Qualifier("deleteExpiredSensorDataStep") Step deleteExpiredSensorDataStep) {
+        return new JobBuilder("deleteExpiredSensorDataJob", jobRepository)
+                .start(deleteExpiredSensorDataStep)
+                .build();
+    }
 
-// @Bean
-// public ListItemReader<SensorData> sensorDataReader(SensorDataRepository
-// sensorDataRepository) {
-// List<SensorData> sensorData = sensorDataRepository.findAll();
-// return new ListItemReader<>(sensorData);
-// }
+    @Bean
+    public Step deleteExpiredSensorDataStep(JobRepository jobRepository,
+            ItemReader<SensorData> sensorDataReader,
+            PlatformTransactionManager transactionManager,
+            SensorDataService sensorDataService) {
+        return new StepBuilder("deleteExpiredSensorDataStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    sensorDataService.deleteExpiredSensorData();
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
 
-// @Bean
-// public ItemProcessor<SensorData, SensorData>
-// sensorDataProcessor(SensorDataService sensorDataService) {
-// return sensorData -> sensorDataService.processSensorDataForWatch(sensorData);
-// }
+    @Bean
+    public ListItemReader<SensorData> sensorDataReader(SensorDataRepository sensorDataRepository) {
+        List<SensorData> sensorData = sensorDataRepository.findAll();
+        return new ListItemReader<>(sensorData);
+    }
 
-// @Bean
-// public ItemWriter<SensorData> sensorDataWriter(SensorDataRepository
-// sensorDataRepository) {
-// return items -> sensorDataRepository.saveAll(items);
-// }
+    @Bean
+    public ItemProcessor<SensorData, SensorData> sensorDataProcessor(SensorDataService sensorDataService) {
+        return sensorData -> sensorDataService.processSensorDataForWatch(sensorData);
+    }
 
-// @Bean
-// public PlatformTransactionManager transactionManager(EntityManagerFactory
-// entityManagerFactory) {
-// return new JpaTransactionManager(entityManagerFactory);
-// }
-// }
+    @Bean
+    public ItemWriter<SensorData> sensorDataWriter(SensorDataRepository sensorDataRepository) {
+        return items -> sensorDataRepository.saveAll(items);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
